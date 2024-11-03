c7c06d489ebbf3fa2b1fcef1074ceb1d33b36b1d
Daniel Judd <ZMAC322@live.rhul.ac.uk>
2024-11-03 13:00:57 +0000

    Renamed frontend Team components to Constructor

---
+++ f1-predictor-frontend/src/components/ConstructorContainer.vue
@@ -0,0 +1,34 @@
+<script>
+
+import ConstructorDisplay from "@/components/ConstructorDisplay.vue";
+import {getFlagURL} from "@/utils/common";
+import {mapGetters} from "vuex";
+
+export default {
+  name: 'ConstructorContainer',
+  components: {ConstructorDisplay},
+  computed: {
+    ...mapGetters(['allConstructors']),
+  },
+  methods: {
+    getFlagURL,
+  },
+};
+
+</script>
+
+<template>
+  <div>
+    <div class="tw-bg-primary">
+      <div>Teams:</div>
+      <div class="tw-flex tw-flex-col tw-gap-2">
+        <ConstructorDisplay v-for="constructor in allConstructors" :key="constructor.code" :constructor="constructor"
+                            :flagURL="getFlagURL(constructor.country)"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ f1-predictor-frontend/src/components/ConstructorDisplay.vue
@@ -0,0 +1,49 @@
+<script>
+
+import {getConciseName} from "@/utils/common";
+
+export default {
+  name: 'TeamDisplay',
+  props: {
+    team: {
+      default: () => ({drivers: {}}),
+      type: Object,
+      required: true
+    },
+    flagURL: {
+      default: "",
+      type: String,
+      required: false
+    },
+  },
+  methods: {
+    getConciseName,
+  }
+};
+</script>
+
+<template>
+  <div
+      class="tw-w-50 tw-h-18 tw-text-f1-white tw-bg-gradient-to-br tw-from-primary-dark tw-to-80% tw-border-2 tw-border-f1-white tw-rounded hover-parent tw-flex tw-flex-col tw-justify-between"
+      :class="[`tw-to-team-${team.shortName}`]">
+    <div class="tw-flex tw-flex-row tw-gap-2 tw-items-center tw-font-medium tw-px-1">
+      <div class="flag-frame tw-my-1 tw-h-5 tw-align-middle tw-border-f1-white tw-rounded">
+        <img :src="flagURL" alt="Flag" class="flag-image"/>
+      </div>
+      {{ team.name }}
+      <div class="tw-ml-auto tw-w-5 tw-h-5 tw-rounded">
+        <span
+            class="material-icons tw-transition-opacity tw-opacity-0 tw-w-full tw-h-full tw-flex tw-items-center tw-justify-center hover-child">
+          tune
+        </span>
+      </div>
+    </div>
+    <div class="tw-bg-amber-300 tw-w-full tw-h-full">
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
