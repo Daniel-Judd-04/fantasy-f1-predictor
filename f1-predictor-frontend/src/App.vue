08480842764a9fff870ecc379e88567b2e75b601
Daniel Judd <ZMAC322@live.rhul.ac.uk>
2024-11-03 19:13:13 +0000

    Improved GrandPrixDisplay Layout

--- f1-predictor-frontend/src/App.vue
+++ f1-predictor-frontend/src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app" class="tw-min-h-screen tw-bg-f1-black">
     <div class="tw-absolute tw-left-0">
-      <div class="tw-flex tw-flex-row tw-gap-4">
+      <div class="tw-flex tw-flex-row tw-gap-4 tw-m-4">
         <TeamContainer/>
         <DriverContainer/>
-        <CircuitDisplay v-if="allCircuits[0] && allGrandsPrix[0]" :circuit="allCircuits[0]"
-                        :grand-prix="allGrandsPrix[0]"/>
+        <GrandPrixDisplay :grand-prix="allGrandsPrix[0]"/>
+        <GrandPrixDisplay :grand-prix="allGrandsPrix[1]"/>
       </div>
     </div>
   </div>
@@ -15,7 +15,7 @@
 import DriverContainer from "@/components/DriverContainer.vue";
 import {mapActions, mapGetters} from "vuex";
 import TeamContainer from "@/components/ConstructorContainer.vue";
-import CircuitDisplay from "@/components/GrandPrixDisplay.vue";
+import GrandPrixDisplay from "@/components/GrandPrixDisplay.vue";
 
 export default {
   name: 'App',
@@ -32,7 +32,7 @@ export default {
     ...mapGetters(['allCircuits', 'allGrandsPrix']),
   },
   components: {
-    CircuitDisplay,
+    GrandPrixDisplay,
     DriverContainer,
     TeamContainer,
   }
--- f1-predictor-frontend/src/components/GrandPrixDisplay.vue
+++ f1-predictor-frontend/src/components/GrandPrixDisplay.vue
@@ -7,17 +7,14 @@ import {
   differenceInWeeks,
   differenceInYears,
   format,
+  isFuture,
   parseISO
 } from 'date-fns';
+import {getCircuitById} from "@/utils/common";
 
 export default {
   name: 'CircuitDisplay',
   props: {
-    circuit: {
-      default: () => ({}),
-      type: Object,
-      required: true
-    },
     grandPrix: {
       default: () => ({}),
       type: Object,
@@ -26,8 +23,17 @@ export default {
   },
   data() {
     return {
-      currentTime: new Date()
+      currentTime: new Date(),
+      circuit: {},
     };
+  },
+  watch: {
+    grandPrix: {
+      immediate: true,
+      handler() {
+        this.circuit = getCircuitById(this.grandPrix.circuit);
+      }
+    }
   },
   computed: {
     formattedDate() {
@@ -36,30 +42,35 @@ export default {
       }
       const targetDate = parseISO(this.grandPrix.startTime);
       const now = this.currentTime;
-      const isFuture = targetDate > now;
+      if (isFuture(targetDate)) {
+        const years = Math.abs(differenceInYears(targetDate, now));
+        const months = Math.abs(differenceInMonths(targetDate, now));
+        const weeks = Math.abs(differenceInWeeks(targetDate, now));
+        const days = Math.abs(differenceInDays(targetDate, now));
+        const hours = Math.abs(differenceInHours(targetDate, now));
+        const minutes = Math.abs(differenceInMinutes(targetDate, now));
 
-      const years = Math.abs(differenceInYears(targetDate, now));
-      const months = Math.abs(differenceInMonths(targetDate, now));
-      const weeks = Math.abs(differenceInWeeks(targetDate, now));
-      const days = Math.abs(differenceInDays(targetDate, now));
-      const hours = Math.abs(differenceInHours(targetDate, now));
-      const minutes = Math.abs(differenceInMinutes(targetDate, now));
-
-      if (years > 1) {
-        return format(targetDate, 'dd/MM/yyyy');
-      } else if (months >= 1) {
-        return isFuture ? `In ${months} month${months > 1 ? 's' : ''}` : `${months} month${months > 1 ? 's' : ''} ago`;
-      } else if (weeks >= 1) {
-        return isFuture ? `In ${weeks} week${weeks > 1 ? 's' : ''}` : `${weeks} week${weeks > 1 ? 's' : ''} ago`;
-      } else if (days >= 1) {
-        return isFuture ? `In ${days} day${days > 1 ? 's' : ''}` : `${days} day${days > 1 ? 's' : ''} ago`;
-      } else if (hours >= 1) {
-        return isFuture ? `In ${hours} hour${hours > 1 ? 's' : ''}` : `${hours} hour${hours > 1 ? 's' : ''} ago`;
-      } else if (minutes >= 1) {
-        return isFuture ? `In ${minutes} minute${minutes > 1 ? 's' : ''}` : `${minutes} minute${minutes > 1 ? 's' : ''} ago`;
-      } else {
-        return "Now";
+        if (years >= 1) {
+          return `In ${years} year${years > 1 ? 's' : ''}`;
+        } else if (months >= 1) {
+          return `In ${months} month${months > 1 ? 's' : ''}`;
+        } else if (weeks >= 1) {
+          return `In ${weeks} week${weeks > 1 ? 's' : ''}`;
+        } else if (days >= 1) {
+          return `In ${days} day${days > 1 ? 's' : ''}`;
+        } else if (hours >= 1) {
+          return `In ${hours} hour${hours > 1 ? 's' : ''}`;
+        } else if (minutes >= 1) {
+          return `In ${minutes} minute${minutes > 1 ? 's' : ''}`;
+        } else {
+          return "Now";
+        }
       }
+
+      return format(targetDate, 'dd/MM/yyyy');
+    },
+    inFuture() {
+      return isFuture(parseISO(this.grandPrix.startTime));
     }
   },
   mounted() {
@@ -76,6 +87,21 @@ export default {
     getRaceLength() {
       return Math.round(this.circuit.length * this.circuit.laps * 10) / 10;
     },
+    getLocation() {
+      let location = '';
+      let locations = [this.circuit.country, this.circuit.locationName, this.circuit.fullName];
+
+      for (let i = 0; i < locations.length; i++) {
+        if (locations[i]) {
+          location += locations[i];
+          if (i < locations.length - 1) {
+            location += ', ';
+          }
+        }
+      }
+
+      return location;
+    },
   }
 }
 
@@ -83,17 +109,30 @@ export default {
 
 <template>
   <div
-      class="tw-border-2 tw-w-96 tw-h-64 tw-bg-gradient-to-bl tw-from-primary-dark tw-to-f1-red tw-to-150% tw-text-f1-white tw-flex tw-flex-col tw-rounded">
-    <div class="tw-ml-auto tw-text-right tw-px-2">
-      <div class="tw-font-bold tw-text-xl">{{ grandPrix.fullName }}</div>
-      <div>{{ circuit.fullName }}, {{ circuit.locationName }}, {{ circuit.country }}</div>
+      class="tw-w-96 tw-h-64 tw-bg-gradient-to-bl tw-from-primary-dark tw-to-f1-red tw-to-150% tw-text-f1-white tw-flex tw-flex-col tw-border-2 tw-border-primary-light tw-rounded">
+    <div class="tw-flex tw-flex-row tw-py-1">
+      <div v-if="grandPrix.sprint" class="tw-h-full tw-pl-2">
+        <span class="material-symbols-outlined">sprint</span>
+      </div>
+      <div class="tw-ml-auto tw-text-right tw-pr-2">
+        <div class="tw-font-bold tw-text-xl">{{ grandPrix.fullName }}</div>
+        <div>{{ getLocation() }}</div>
+      </div>
     </div>
-    <div class="tw-w-full tw-h-full tw-bg-primary-dark tw-flex tw-flex-col">
-      <div class="tw-flex tw-flex-row tw-h-full tw-px-2 tw-gap-2 tw-mt-2">
+    <div class="tw-w-full tw-h-full tw-bg-primary-dark tw-flex tw-flex-col tw-gap-2 tw-py-2">
+      <div class="tw-flex tw-text-xs tw-flex-row tw-justify-around">
+        <div>Laps: {{ circuit.laps }}</div>
+        <div>Turns: {{ circuit.corners }}</div>
+        <div>DRS: {{ circuit.drsZones }}</div>
+        <div>Length: {{ circuit.length }} km</div>
+        <div>Race: {{ getRaceLength() }} km</div>
+      </div>
+      <div class="tw-flex tw-flex-row tw-h-full tw-px-2 tw-gap-2">
         <div class="tw-h-full tw-w-full tw-bg-white tw-rounded">
           <!--IMAGE-->
         </div>
-        <div class="tw-ml-auto tw-w-28 tw-flex tw-flex-col tw-justify-around">
+        <div v-if="!inFuture"
+             class="tw-font-thin tw-border-y-2 tw-border-primary-light tw-ml-auto tw-w-28 tw-flex tw-flex-col tw-justify-around">
           <div class="tw-flex">
             <span class="material-symbols-outlined tw-mr-auto">heat</span>
             <div>{{ grandPrix.airTemperature }}°C</div>
@@ -116,17 +155,9 @@ export default {
           </div>
         </div>
       </div>
-      <div class="tw-w-full tw-flex tw-flex-col">
-        <div class="tw-flex tw-flex-row tw-justify-around">
-          <div>Circuit Length: {{ circuit.length }} km</div>
-          <div>Race Distance: {{ getRaceLength() }} km</div>
-        </div>
-        <div class="tw-flex tw-flex-row tw-justify-around">
-          <div>{{ circuit.laps }} Laps</div>
-          <div>{{ circuit.corners }} Corners</div>
-          <div>{{ circuit.drsZones }} DRS Zones</div>
-        </div>
-      </div>
+    </div>
+    <div v-if="inFuture" class="tw-font-bold tw-bg-primary">
+      {{ formattedDate }}
     </div>
   </div>
 </template>
