763490adffea25820e6e25322510caa45125133c
Daniel Judd <ZMAC322@live.rhul.ac.uk>
2024-11-05 15:24:24 +0000

    Created 2 Button classes for a unified look.

---
+++ f1-predictor-frontend/src/components/common/CloseButton.vue
@@ -0,0 +1,19 @@
+<script>
+
+export default {
+  name: 'CloseButton',
+  methods: {
+    emitClose() {
+      this.$emit('close');
+    }
+  }
+}
+
+</script>
+
+<template>
+  <button @click="emitClose()"
+          class="tw-px-2 tw-h-10 tw-border-1 tw-border-f1-red tw-rounded tw-bg-f1-red tw-bg-opacity-15 hover:tw-bg-opacity-25 tw-transition-opacity">
+    <slot></slot>
+  </button>
+</template>
---
+++ f1-predictor-frontend/src/components/common/ContinueButton.vue
@@ -0,0 +1,23 @@
+<script>
+
+export default {
+  name: 'ContinueButton',
+  methods: {
+    emitContinue() {
+      this.$emit('continue');
+    }
+  }
+}
+
+</script>
+
+<template>
+  <button @click="emitContinue()"
+          class="tw-px-4 tw-h-10 tw-border-1 tw-border-primary-light tw-rounded tw-bg-primary-light tw-bg-opacity-15 hover:tw-bg-opacity-25 tw-transition-opacity">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+
+</style>
