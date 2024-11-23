ce264b1d12e9beb66777434920a6890eecf8ce1e
Daniel Judd <ZMAC322@live.rhul.ac.uk>
2024-11-23 22:44:01 +0000

    Added 'Edit' button to TeamDisplay. Improved icons for TeamDisplay.

--- f1-predictor-frontend/src/components/display/TeamDisplay.vue
+++ f1-predictor-frontend/src/components/display/TeamDisplay.vue
@@ -27,20 +27,26 @@ export default {
       this.comparativeTeam = this.userTeams.find(team => `${team.teamName}-${team.teamOwner}` === document.getElementById('comparativeTeam').value);
       this.$emit('updateComparativeTeam', this.comparativeTeam);
     },
+    editTeams() {
+      this.$emit('editArray', this.userTeams);
+    }
   },
 }
 </script>
 
 <template>
   <div v-if="userTeams && comparativeTeam"
-       class="container tw-flex tw-flex-col tw-h-64 tw-w-1/3 tw-outline tw-outline-1 tw-outline-primary-light -tw-outline-offset-1 tw-rounded-lg tw-text-f1-white tw-bg-gradient-to-bl tw-to-f1-red tw-to-200% tw-from-primary-dark">
-    <div class="tw-flex tw-flex-row tw-pt-1 tw-px-2 tw-border-b-1 tw-border-primary-light">
+       class="container hover-parent tw-flex tw-flex-col tw-h-64 tw-w-1/3 tw-outline tw-outline-1 tw-outline-primary-light -tw-outline-offset-1 tw-rounded-lg tw-text-f1-white tw-bg-gradient-to-bl tw-to-f1-red tw-to-200% tw-from-primary-dark">
+    <div class="tw-flex tw-flex-row tw-items-center tw-pl-2 tw-pr-1 tw-border-b-1 tw-border-primary-light">
       <select id="comparativeTeam" @change="updateComparativeTeam"
-              class="tw-appearance-none tw-text-lg tw-border-0 tw-bg-primary-light tw-bg-opacity-0 tw-font-bold tw-text-f1-white">
+              class="tw-appearance-none tw-text-lg tw-pt-1 tw-border-0 tw-bg-primary-light tw-bg-opacity-0 tw-font-bold tw-text-f1-white">
         <option :value="`${team.teamName}-${team.teamOwner}`" v-for="team in userTeams" :key="team.code">
           {{ `${team.teamName} (${team.teamOwner})` }}
         </option>
       </select>
+      <div class="hover-child tw-mt-0.5 tw-mr-0.5 tw-transition-opacity tw-cursor-pointer tw-ml-auto tw-flex tw-items-center">
+        <span @click="editTeams()" class="material-symbols-outlined tw-font-light">edit_note</span>
+      </div>
     </div>
     <div class="tw-h-full tw-flex tw-flex-col tw-gap-2 tw-bg-primary-dark tw-rounded-b-lg">
       <div class="tw-flex tw-justify-between tw-px-2 tw-pt-0.5">
@@ -48,18 +54,21 @@ export default {
         <div class="tw-font-medium">Free Transfers: {{ comparativeTeam.freeTransfers }}</div>
       </div>
       <div class="tw-flex tw-justify-between tw-px-2 tw-pb-0.5 tw-text-sm tw-border-b-1 tw-border-primary-light">
-        <div class="tw-flex tw-items-center tw-gap-0.5"><span
-            class="material-symbols-outlined tw-text-base tw-mb-0.5">joystick</span>{{ comparativeTeam.averageFantasyPoints.toFixed(0) }}
+        <div class="tw-flex tw-items-center tw-gap-0.5">
+          <span class="material-symbols-outlined tw-text-base tw-font-light tw-mb-0.5">joystick</span>
+          {{ comparativeTeam.averageFantasyPoints.toFixed(0) }}
         </div>
-        <div class="tw-flex tw-items-center tw-gap-0.5"><span class="material-symbols-outlined tw-text-base tw-mb-0.5">timer</span>{{
-            comparativeTeam.averageRecentPoints.toFixed(0)
-          }}
+        <div class="tw-flex tw-items-center tw-gap-0.5">
+          <span class="material-symbols-outlined tw-text-base tw-font-light tw-mb-0.5">timer</span>
+          {{ comparativeTeam.averageRecentPoints.toFixed(0) }}
         </div>
-        <div class="tw-flex tw-items-center tw-gap-0.5"><span class="material-symbols-outlined tw-text-base tw-mb-0.5">trophy</span>{{ comparativeTeam.averagePoints.toFixed(0) }}
+        <div class="tw-flex tw-items-center tw-gap-0.5">
+          <span class="material-symbols-outlined tw-text-base tw-font-light tw-mb-0.5">trophy</span>
+          {{ comparativeTeam.averagePoints.toFixed(0) }}
         </div>
-        <div class="tw-flex tw-items-center tw-gap-0.5"><span class="material-symbols-outlined tw-text-base tw-mb-0.5">tools_level</span>{{
-            comparativeTeam.consistency.toFixed(0)
-          }}
+        <div class="tw-flex tw-items-center tw-gap-0.5">
+          <span class="material-symbols-outlined tw-text-base tw-font-light tw-mb-0.5">tools_level</span>
+          {{ comparativeTeam.consistency.toFixed(0) }}
         </div>
       </div>
       <div v-if="comparativeTeam.drivers" class="tw-w-full tw-mt-auto tw-flex tw-justify-center tw-px-1 tw-gap-2">
